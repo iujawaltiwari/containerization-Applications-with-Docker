1d502e4a94d3d8039ecd0325d7ad4fbe628b31fe
Ubuntu <ubuntu@ip-172-31-41-131.ap-south-1.compute.internal>
2024-02-24 19:05:24 +0000

    java-simple-Dockerfile

---
+++ java-small-project-for-test/Hello.java
@@ -0,0 +1,13 @@
+import java.utils.*;
+
+
+public class Hello{
+
+    public static void main(String[] args){
+       System.out.println("Hello Dosto");
+        Date currentDate = new Date();
+       System.out.println("today's date is: " + currentDate);
+    
+    }
+}
+
